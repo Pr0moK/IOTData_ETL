1dd8fa5815e8dddb40f5370ffef12c70f13c0eb2
Pr0moK <pr0mok1234@gmail.com>
2026-03-18 22:21:19 +0100

    added groupby method

--- Util.py
+++ Util.py
@@ -7,8 +7,9 @@ class iot:
         self.df = pd.read_csv("energydata_complete.csv")
         df = self.df
         df["date"] = pd.to_datetime(df["date"])
+        df["month"] = df["date"].dt.month
         df["time"] = df["date"].dt.strftime("%H:%M")
-        df["date"] = df["date"].dt.strftime("%Y-%m-%d")
+        df["data"] = df["date"].dt.strftime("%Y-%m-%d")
 
 
     def GetDaylyRv1(self,month):
@@ -16,9 +17,9 @@ class iot:
         if month > "5" or month < "1":
             raise ValueError("Invalid month")
         else:
-            df = df[(df["date"] >= f"2016-0{month}-01") & (df["date"] <= f"2016-0{month}-31")]
-            df["date"] = pd.to_datetime(df["date"])
+            df = df[(df["data"] >= f"2016-0{month}-01") & (df["data"] <= f"2016-0{month}-31")]
             plt.bar(df["date"].dt.strftime("%d").str.replace("0",""), df["rv1"])
+            plt.xticks(rotation=90)
             plt.show()
 
     def GetHourlyWindSpeed(self,month,day):
@@ -36,21 +37,19 @@ class iot:
             plt.show()
 
     def MeanTemperature(self,typ):
-        T_out = []
         df = self.df
         df["date"] = pd.to_datetime(df["date"])
         for miesiac in range(1, 6):
             if typ == 1:
-                tempout = df[df["date"].dt.strftime("%m") == f"0{miesiac}"]["T_out"].mean()
+                tempout = df.groupby(["month"])["T_out"].mean()
                 plt.title("Mean Temperature")
             elif typ == 2:
-                tempout = df[df["date"].dt.strftime("%m") == f"0{miesiac}"]["T_out"].max()
+                tempout = df.groupby(["month"])["T_out"].max()
                 plt.title("Max Temperature")
             else:
-                tempout = df[df["date"].dt.strftime("%m") == f"0{miesiac}"]["T_out"].min()
+                tempout = df.groupby(["month"])["T_out"].min()
                 plt.title("Min Temperature")
-            T_out.append(tempout)
-        plt.bar(range(1,6), T_out)
+        plt.bar(range(1,6), tempout)
         plt.ylabel("Temperature (C)")
         plt.xlabel("Month")
         plt.show()
@@ -63,8 +62,8 @@ class iot:
                 day = "0" + str(day)
 
         df = self.df
-        Temp = df[df["date"] == f"2016-0{month}-{day}"][["T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9"]]
-        time = df[df["date"] == f"2016-0{month}-{day}"]["time"]
+        Temp = df[df["data"] == f"2016-0{month}-{day}"][["T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9"]]
+        time = df[df["data"] == f"2016-0{month}-{day}"]["time"]
         Temp.value_counts()
         Temp.columns = ["kitchen", "living room", "laundry room", "office room", "bathroom", "outside north",
                         "ironing room", "teenager room", "parents room"]
